28d0f0f0bf1fdfeb28acd73d3f70ad68c5f6fa9d
Tasnuba190135 <tasnuba.tasnim2000@gmail.com>
2024-08-20 00:37:02 +0600

    added

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -21,6 +21,8 @@ function calculateTriangleArea(){
     // show triangle area
     const areaSpan = document.getElementById('triangle-area');
     areaSpan.innerText = area;
+
+    addToCalculationEntry('Triangle',area);
 }
 function calculateRectangleArea(){
     // console.log('rect');
@@ -37,6 +39,13 @@ function calculateRectangleArea(){
     const length = parseFloat(lengthValueText);
     console.log(length);
 
+    // validate input
+    if(isNaN(width)|| isNaN(length)){
+        // console.log('Width is not a number');
+        alert('Please insert a number');
+        return;
+    }
+
     // calculate rectangular area
     const area = width * length;
     console.log(area);
@@ -44,6 +53,8 @@ function calculateRectangleArea(){
     // show rectangle area
     const rectangleAreaSpan = document.getElementById('rectangle-area');
     rectangleAreaSpan.innerText = area;
+
+    addToCalculationEntry('Rectangle',area);
 
 }
 
@@ -56,15 +67,27 @@ function calculateParallelogramArea(){
     const height = getInputValue('parallelogram-height');
     // console.log(height);
 
+    // validate
+    if(isNaN(base) || isNaN(height)){
+        alert('Please insert number');
+        return;
+    }
+
     const area = base * height;
     setElementInnerText('parallelogram-area',area);
+
+    // add to calculation entry
+    addToCalculationEntry('Parallelogram',area);
 }
 
 function calculateEllipseArea(){
     const majorRadius = getInputValue('ellipse-major-radius');
     const minorRadius = getInputValue('ellipse-minor-radius');
     const area = 3.14 * majorRadius * minorRadius;
-    setElementInnerText('ellipse-area',area);
+    const areaTwoDecimal = area.toFixed(2);
+    setElementInnerText('ellipse-area',areaTwoDecimal);
+
+    addToCalculationEntry('Ellipse',areaTwoDecimal);
 }
 
 // reusable get input value in number
@@ -80,8 +103,39 @@ function getInputValue(fieldId){
 function setElementInnerText(elementId, area){
     const element = document.getElementById(elementId);
     element.innerText = area;
-
-
-   
+  
 }
+
+// add to calculation entry
+/* 
+1.Get the element where you want to add the dynamic html
+2.create an element you want to add
+3.if needed add some class
+4. set inner HTML. it could be dynamic Template String
+5.append the created element as a child of the parent
+ */
+function addToCalculationEntry(areaType, area){
+    console.log(areaType+ ' ' +area);
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList.add('my-4')
+    // p.innerHTML = areaType + ' ' + area;
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`
+
+    calculationEntry.appendChild(p);
+
+
+}
+
+// Data Validation
+/* 
+1. set the proper type of the input field(number,data,email)
+2.check type using typeof
+3. NaN means: Not a number .isNan is checking whether the input is not a number or not
+
+
+ */
 
